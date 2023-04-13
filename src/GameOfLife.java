346ec32260ed5220dadca7cff00572cf6cd88f8b
priitrs <priitrs@gmail.com>
2023-04-13 09:44:51 +0300

    refactor

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,8 +1,9 @@
 import java.util.Arrays;
 
 public class GameOfLife {
-
     static int sum;
+    static int[][] nextState;
+
     public static void main(String[] args) {
         int[][] input = new int[][]{
                 {0, 0, 0, 0, 0, 0, 0},
@@ -13,13 +14,15 @@ public class GameOfLife {
                 {0, 1, 1, 1, 1, 0, 0},
                 {0, 1, 1, 1, 1, 0, 0},
         };
-        int counter = 0;
 
-        while (counter < 10) {
+        printOutIterations(input, 10);
+    }
+
+    private static void printOutIterations(int[][] input, int numberOfIterations) {
+        int counter = 0;
+        while (counter < numberOfIterations) {
             input = getNextState(input);
-            for (int[] ints : input) {
-                System.out.println(Arrays.toString(ints));
-            }
+            for (int[] ints : input) System.out.println(Arrays.toString(ints));
             System.out.println();
             counter++;
             try {
@@ -30,14 +33,18 @@ public class GameOfLife {
     }
 
     public static int[][] getNextState(int[][] input) {
-        int[][] nextState = new int[input.length][input[0].length];
+        nextState = new int[input.length][input[0].length];
         for (int i = 0; i < input.length; i++) {
             for (int j = 0; j < input[i].length; j++) {
                 findSum(input, i, j);
-                nextState[i][j] = ((sum == 4 && input[i][j] == 1) || sum == 3) ? 1 : 0;
+                setNextValue(i, j, input[i][j]);
             }
         }
         return nextState;
+    }
+
+    private static void setNextValue(int i, int j, int currentValue) {
+        nextState[i][j] = ((sum == 4 && currentValue == 1) || sum == 3) ? 1 : 0;
     }
 
     private static void findSum(int[][] input, int i, int j) {
